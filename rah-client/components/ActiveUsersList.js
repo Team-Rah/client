5d2ae0dee5fb338d0351ef4faabb22b87713d223
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-05-10 20:22:32 -0400

    added active users list to lobby, adjusted styling

--- rah-client/components/ActiveUsersList.js
+++ rah-client/components/ActiveUsersList.js
@@ -2,27 +2,34 @@ import Container from '@mui/material/Container';
 import { useContext, useEffect, useState } from 'react';
 import { SocketContext } from '../socket/socket';
 import uuid from 'react-uuid';
+import { userState } from '../_states/tokenState';
+import { useRecoilValue } from 'recoil';
+// remove
+import activeUsers from '../pages/_sampleData/activeUsers';
 
 // !! TO DO -> switch from props to users list
-export default function ActiveUsersList(props) {
+export default function ActiveUsersList() {
 
   const socket = useContext(SocketContext);
+  const userData = useRecoilValue(userState);
   const [usersList, setUsersList] = useState([]);
 
-  useEffect(() => {
-    socket.emit('join-room', {userName: uuid()}, 'lobby');
-    socket.on('receive-lobby', (users) => {
-      setUsersList(users);
-    })
-    socket.on('error', (err) => {
-      console.error(err);
-    })
-  }, [])
+  // useEffect(() => {
+  //   socket.emit('join-room', {userName: uuid()}, 'lobby');
+  //   socket.on('receive-lobby', (users) => {
+  //     setUsersList(users);
+  //     console.log(usersList, 'usersList');
+  //   })
+  //   socket.on('error', (err) => {
+  //     console.error(err);
+  //   })
+  // }, [])
 
   return (
-    <Container maxWidth={false} id="activeUsers-container">
-        <h3>Online Players</h3>
-        {props.users.map((user) => (
+    <Container disableGutters={true} maxWidth={false} id="activeUsers-container">
+        <div style={{textAlign: 'center', marginRight: '30px'}}><h3>Online Players</h3></div>
+        {/* {!usersList.length ? <div>No active users</div> : usersList.map((user) => ( */}
+        {activeUsers.map((user) => (
           <div key={JSON.stringify(user)} maxWidth={false} className="activeUser-item">
 
             <div id="activeUsersList-container">
@@ -34,6 +41,6 @@ export default function ActiveUsersList(props) {
             </div>
           </div>
         ))}
-    </Container>
+      </Container>
   )
 }
